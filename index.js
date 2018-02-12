5230f033994219aaf887a888e7a7a730bf10fde1
toledoal <toledoal@users.noreply.github.com>
2018-02-12 11:33:28 -0600

    default port

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
